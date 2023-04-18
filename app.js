32eb867b1ca417a0e8e50fe19ab0db673456a0ba
gisa chris <gisachris24@gmail.com>
2023-04-18 03:55:10 -0200

    making chnages to the mobile navigation part-1

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-const hamButton = document.querySelector('#menuToggle');
-//add an event listener
-hamButton.addEventListener('click',function(){
- 
-})
+const navToggle = document.querySelector('#menuToggle');
+const navIcon = document.querySelectorAll('#menuToggle .bar')
+const nav = document.querySelector('.navigation');
+const logo = document.querySelector('.logo-text');
+const navItem = document.querySelectorAll('.navigation li .nav-text');
+
+navToggle.addEventListener('click', () =>{
+  nav.classList.toggle('open');
+  logo.classList.toggle('logo-dis');
+   (navIcon).forEach(icon =>{
+      icon.classList.toggle('hidden');
+    });
+   });
+
+
+(navItem).forEach( link =>{
+   link.addEventListener('click',()=>{
+    nav.classList.toggle('open');
+    logo.classList.remove('logo-dis');
+    (navIcon).forEach(icon =>{
+      navIcon[0].classList.remove('hidden');
+    navIcon[1].classList.add('hidden');
+    });
+   });
+});
+
+window.addEventListener('scroll', () => {
+  logo.classList.remove('logo-dis');
+  nav.classList.remove('open');
+  navIcon.forEach(icon =>{
+    navIcon[0].classList.remove('hidden');
+    navIcon[1].classList.add('hidden');
+  });
+});
