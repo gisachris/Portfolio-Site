9304faec1ef04fa149dacf6476432a6d45999b41
gisa chris <gisachris24@gmail.com>
2023-11-03 17:44:35 +0200

    Implement hover functionality for featured projects

--- src/components/featured.jsx
+++ src/components/featured.jsx
@@ -1,21 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import projects from "../data/featured.json";
+import textCleanup from "../helpers/textCleanup";
 import '../styles/css/featured.css';
 
 const FeaturedApps = () => {
   const [featuredProjects, setFeaturedProjects] = useState(null);
+  const projectRefs = useRef([]);
 
   useEffect(() =>{
-    if(projects){
+    if (projects) {
       setFeaturedProjects(projects);
     }
-  })
+  });
 
-  return(
+  const overlayInfo = (index) => {
+    projectRefs.current[index].classList.add('showProjectDetail');
+  }
+
+  const removeOverlay = (index) => {
+    projectRefs.current[index].classList.remove('showProjectDetail');
+  }
+
+  const showDataModal = (id) => {
+    console.log(id);
+  }
+
+  return (
     <div id="Portfolio">
       <section className="portfolioHeader">
         <h1 className="headerText">MY WORKS</h1>
         <span className="featuredText">Featured Works</span>
+        <span className="usageNotice">Click on a section or Project Below for more Details</span>
       </section>
       <section className="projectTypeSelectorsHolder">
         <span className="typeSelector">All</span>
@@ -24,16 +39,29 @@ const FeaturedApps = () => {
         <span className="typeSelector">Website Development</span>
       </section>
       <section className="projectsHolder projects-grid-container">
-        {featuredProjects && featuredProjects.map(project => (
-          <div className={`project grid-item grid-item-${project.id && project.id}`}>
-            {project.images.large && <img src={`${project.images.large}`} alt="background__image_project"  className="projectImage"/>}
+        {featuredProjects && featuredProjects.map((project, index) => (
+          <div
+            className={`project grid-item grid-item-${project.id && project.id}`}
+            key={project.id}
+            onMouseEnter={() => overlayInfo(index)}
+            onMouseLeave={() => removeOverlay(index)}
+            onClick={() => showDataModal(project.id)}
+          >
+            <div className="overlayInfo" ref={(element) => projectRefs.current[index] = element}>
+              <div className="imfoSection">
+                <h1 className="projectName">{project.name}</h1>
+                <span className="projectDetails">Click to view Project Description.</span>
+              </div>
+            </div>
+            {project.images.small && <img src={project.images.small} alt="background__image_project" className="projectImage" />}
+            {(!project.images.small && project.images.large) && <img src={project.images.large} alt="background__image_project" className="projectImage" />}
           </div>
         ))}
         {featuredProjects === null && (
           <div className="noProjectsAlert">
             <div className="alert">
               <p>
-                fetching Project Data Is taking longer than Expected.<br />Please Try Accessing them through my Github Account
+                Fetching Project Data Is taking longer than Expected.<br />Please Try Accessing them through my Github Account
               </p>
               <a href="https://github.com/gisachris" className="account">
                 Github Link
@@ -43,7 +71,7 @@ const FeaturedApps = () => {
         )}
       </section>
     </div>
-  )
+  );
 }
 
 export default FeaturedApps;
