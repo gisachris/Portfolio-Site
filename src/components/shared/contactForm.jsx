6edae382f48634e5ff1f0de599092f0fe1ac2508
gisa chris <gisachris24@gmail.com>
2023-11-09 05:24:48 +0200

    perform cleanup

--- src/components/shared/contactForm.jsx
+++ src/components/shared/contactForm.jsx
@@ -25,7 +25,6 @@ const ContactForm = () => {
     const serviceId = import.meta.env.VITE_SERVICE_ID;
     const templateId = import.meta.env.VITE_TEMPLATE_ID;
     const publicKey = import.meta.env.VITE_PUBLIC_KEY;
-    console.log(`${serviceId}, ${templateId}, ${publicKey}`);
   
     emailjs.sendForm(`${serviceId}`, `${templateId}`, form.current, `${publicKey}`)
       .then(() => {
